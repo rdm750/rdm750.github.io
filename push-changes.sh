560bd14234ecb70e1dd4999215b26fedb284b313
rdm750 <rdm750>
2016-05-13 14:32:43 -0400

    initial commit

--- push-changes.sh
+++ push-changes.sh
@@ -3,6 +3,4 @@ git commit -m 'initial commit'
 git status
 git push origin master
 date
-On branch master
-Your branch is up-to-date with 'origin/master'.
-nothing to commit, working directory clean
+git status
