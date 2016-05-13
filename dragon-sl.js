26be319b940cfb5fca07b9bee6852235cd708edd
rdm750 <rdm750>
2016-05-12 23:38:24 -0400

    initial commit

---
+++ dragon-sl.js
@@ -0,0 +1,25 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+
+while(slaying)
+{
+    if(youHit)
+    {
+    console.log("hit a dragon");
+    totalDamage += damageThisRound;
+        if(totalDamage>=4)
+        { 
+            console.log("the player slew the dragon");
+            slaying =false;
+        }
+        else
+            youHit = Math.floor(Math.random() * 2);    
+    }
+    else
+    console.log("dragon defeated you");
+    
+    
+    slaying = false;
+    }
