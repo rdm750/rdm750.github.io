bf8092e9427b4738f41fe81d5a2456a20736ca53
rdm750 <rdm750>
2016-05-18 19:15:02 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+        $('<body></body>').append('<div class="div">' + '</div>');
+$('<body></body>').append('<ul class="item">' + '</ul>');       
+for(var i=0;i<=10;i++)
+{
+       $('<body></body>').append('<li class="div">' + i  + '</li>');
+        
+ }       
+    
+    });
