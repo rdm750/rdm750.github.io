4ee239475d8aedc0ced9574a74796b1dcc93a7be
rdm750 <rdm750>
2016-05-19 15:16:54 -0400

    initial commit

--- test.js
+++ test.js
@@ -2,3 +2,11 @@ var div  = document.body.children[0]
  var span = document.createElement('span')
   span.innerHTML = 'A new span!'
   div.appendChild(span)
+
+document.write("<table>")
+for(var i=0;i<100;i++){
+document.write("<tr>"+i+"</tr>")
+}
+document.write("</table")
+
+
