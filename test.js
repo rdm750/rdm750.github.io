21f2e660b6579656718eafbe15126a08d815cbd1
rdm750 <rdm750>
2016-05-18 21:47:09 -0400

    initial commit

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var $newdiv1 = $( "<div id='object1'/>" ),
   newdiv2 = document.createElement( "div" ),
-  existingdiv1 = document.getElementById( "div" );
+  existingdiv1 = document.getElementById( "left" );
  
 $( "body" ).append( $newdiv1, [ newdiv2, existingdiv1 ] ); 
