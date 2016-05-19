923e63e1219e61f9e8686e15289419ec99e7e610
rdm750 <rdm750>
2016-05-19 00:06:38 -0400

    initial commit

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-var $newdiv1 = "<div id='test'/>";
-  $newdiv2 = document.createElement( "div" );
-  existingdiv1 = document.getElementById( "left" );
- 
-$( "test" ).append( $newdiv1, [ newdiv2, existingdiv1 ] ); 
+var newdiv1 = "<div id='div'/>";
+  var newdiv2 = document.createElement( "div" );
+  existingdiv1 = document.body.children[0];
+ existingdiv1..append(newdiv1); 
