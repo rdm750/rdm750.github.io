05488b700afc70b3c8027ed55674c882025aae14
rdm750 <rdm750>
2016-05-22 21:03:15 -0400

    initial commit

---
+++ python/unique_substrings.py
@@ -0,0 +1,22 @@
+'''
+This prints unique substrings for a given string
+pass by cli in unix mac osx
+unique_substring.py ababba
+
+'''
+# your code goes here
+#test= 'abab'
+#for i in range(len(test)):
+#	for k in range(len(test)-i):
+#		print test[i:i+k+1]
+import sys
+
+def uniq_substring(test):
+	lista=[]
+	[lista.append(test[i:i+k+1]) for i in range(len(test)) for k in range(len(test)-i) if test[i:i+k+1] not in lista and test[i:i+k+1][::-1] not in lista]
+	print lista
+
+uniq_substring('rohit')
+uniq_substring('abab')
+uniq_substring(sys.argv[1])
+
