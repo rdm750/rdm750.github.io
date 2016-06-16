ae7aaa00d273bd4a8f31f78f4dfe6fdc45e1493d
rdm750 <rdm750>
2016-06-16 12:49:24 -0400

    initial commit

--- python/gray_code_generator.py
+++ python/gray_code_generator.py
@@ -4,14 +4,23 @@ the n-1 bit code, with 0 prepended to each word, followed by
 the n-1 bit code in reverse order, with 1 prepended to each word.
 Rohit Malgaonkar
 '''
+import os
+import sys
+
+F=[[]]*100
 
 def graycode(n):
 	if n==1:
+		F[n]=['0','1']
 		return ['0','1']
 	else:
-		nbit=map(lambda x:'0'+x,graycode(n-1))+map(lambda x:'1'+x,graycode(n-1)[::-1])
+		if F[n]!=[]:
+			return F[n]
+		else:
+			nbit=map(lambda x:'0'+x,graycode(n-1))+map(lambda x:'1'+x,graycode(n-1)[::-1])
+			F[n]=nbit	
 		return nbit
 
-for i in xrange(1,7):
-	print map(int,graycode(i))
+print map(int,graycode(int(sys.argv[1])))
+print F
 
