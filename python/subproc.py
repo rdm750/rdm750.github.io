82062386ad37bf6fed0f0a354f3cd5470b8762f6
Rohit Malgaonkar <rdm750@gmail.com>
2018-11-02 22:03:50 -0400

    Update subproc.py

--- python/subproc.py
+++ python/subproc.py
@@ -1,10 +1,10 @@
 import subprocess
 
 
-proc = subprocess.Popen(['ssh','medhamalgaonkar@10.0.0.7'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+proc = subprocess.Popen(['ssh','user@192.168.1.1'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 proc.communicate('Atmaram123\n')
 
-#proc.stdin.write("Atmaram123\n")
+#proc.stdin.write("password\n")
 #proc.stdin.write("ls -ltr\n")
 
 
